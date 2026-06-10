ac4de85c48b651daa3ea7617d959ad6fb9c56b81
wazir655 <ahmadwazir@mhs.usk.ac.id>
2026-06-11 03:02:49 +0700

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,18 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include "word.h"
+
+void heapSortAscending(
+    WordFreq arr[],
+    int n);
+
+void heapSortDescending(
+    WordFreq arr[],
+    int n);
+
+void insertionSort(
+    WordFreq arr[],
+    int n);
+
+#endif
